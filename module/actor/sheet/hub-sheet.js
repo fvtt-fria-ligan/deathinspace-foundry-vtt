a1e97dcd2c05c8c1d4642fb82c0c5950bbe38568
mcglincy <mattmcglincy@gmail.com>
2021-05-07 15:49:31 -0500

    Display items in cargo tab.

--- module/actor/sheet/hub-sheet.js
+++ module/actor/sheet/hub-sheet.js
@@ -1,23 +1,31 @@
 /**
  * @extends {ActorSheet}
  */
- export class DISHubSheet extends ActorSheet {
+export class DISHubSheet extends ActorSheet {
 
-    /** @override */
-    static get defaultOptions() {
-      return mergeObject(super.defaultOptions, {
-        classes: ["deathinspace", "sheet", "actor", "hub"],
-        template: "systems/deathinspace/templates/actor/hub-sheet.html",
-        width: 730,
-        height: 680,
-        tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "specifics"}],
-        dragDrop: [{dragSelector: ".item-list .item", dropSelector: null}]
-      });
-    }
-  
-    /** @override */
-    activateListeners(html) {
-      super.activateListeners(html);
-    }
+  /** @override */
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["deathinspace", "sheet", "actor", "hub"],
+      template: "systems/deathinspace/templates/actor/hub-sheet.html",
+      width: 730,
+      height: 680,
+      tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "specifics"}],
+      dragDrop: [{dragSelector: ".item-list .item", dropSelector: null}]
+    });
   }
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+  }
+
+  /** @override */
+  getData() {
+    const data = super.getData();
+    // TODO: filter items etc
+    data.actor.data.cargo = data.items;
+    return data;
+  }  
+}
   
