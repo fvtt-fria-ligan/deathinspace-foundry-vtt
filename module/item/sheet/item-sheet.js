a95e69f677fd46c919a8719524983c5e08f3b232
mcglincy <mattmcglincy@gmail.com>
2021-05-17 14:58:41 -0500

    Add specific item sheets for cargo and hub module.

--- module/item/sheet/item-sheet.js
+++ module/item/sheet/item-sheet.js
@@ -9,7 +9,6 @@ export class DISItemSheet extends ItemSheet {
     static get defaultOptions() {
       return mergeObject(super.defaultOptions, {
         classes: ["deathinspace", "sheet", "item"],
-        template: "systems/deathinspace/templates/item/item-sheet.html",
         width: 730,
         height: 680,
         tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "data"}],
@@ -18,9 +17,20 @@ export class DISItemSheet extends ItemSheet {
     }
   
     /** @override */
+    get template() {
+      const path = "systems/deathinspace/templates/item";
+      if (["cargo", "hubModule"].includes(this.item.data.type)) {
+        // specific item-type sheet
+        return `${path}/${this.item.data.type}-sheet.html`;
+      } else {
+        // generic item sheet
+        return `${path}/item-sheet.html`;
+      }
+    }
+
+    /** @override */
     activateListeners(html) {
       super.activateListeners(html);
     }
-
   }
   
