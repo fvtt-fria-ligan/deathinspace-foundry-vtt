4eea62ae530ae5012879566ad9b268a259e3da96
mcglincy <mattmcglincy@gmail.com>
2021-10-17 19:20:43 -0500

    Accumulate same-type items.

--- module/actor/sheet/character-sheet.js
+++ module/actor/sheet/character-sheet.js
@@ -21,4 +21,35 @@ export class DISCharacterSheet extends DISActorSheet {
   activateListeners(html) {
     super.activateListeners(html);
   }
+
+  /** @override */
+  getData() {
+    const superData = super.getData();
+    const data = superData.data;
+    data.config = CONFIG.DIS;
+
+    // Ability Scores
+    // for (let [a, abl] of Object.entries(data.data.abilities)) {
+    //   const translationKey = CONFIG.MB.abilities[a];
+    //   abl.label = game.i18n.localize(translationKey);
+    // }
+
+    this.prepareCharacterItems(data);
+
+    console.log(superData);
+    return superData;
+  }  
+
+  prepareCharacterItems(sheetData) {
+    const byName = (a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
+    sheetData.data.origin = sheetData.items.filter(item => item.type === CONFIG.DIS.itemTypes.origin).pop();
+    sheetData.data.originBenefits = sheetData.items.filter(item => item.type === CONFIG.DIS.itemTypes.originBenefit).sort(byName);
+    sheetData.data.cosmicMutations = sheetData.items.filter(item => item.type === CONFIG.DIS.itemTypes.cosmicMutation).sort(byName);
+    sheetData.data.voidCorruptions = sheetData.items.filter(item => item.type === CONFIG.DIS.itemTypes.voidCorruption).sort(byName);
+    sheetData.data.weapons = sheetData.items.filter(item => item.type === CONFIG.DIS.itemTypes.weapon).sort(byName);
+    sheetData.data.armor = sheetData.items.filter(item => item.type === CONFIG.DIS.itemTypes.armor).sort(byName);
+    sheetData.data.equipment = sheetData.items.filter(item => item.type === CONFIG.DIS.itemTypes.equipment).sort(byName);
+
+    //sheetData.data.belongings = [...sheetData.data.weapons, ...sheetData.data.armor, ...sheetData.data.equipment].sort(byName);
+  }
 }
