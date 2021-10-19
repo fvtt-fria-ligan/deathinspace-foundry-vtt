137b1bd53ef2dbd5d35df19b648e4d9991605370
mcglincy <mattmcglincy@gmail.com>
2021-10-18 23:08:33 -0500

    Get hub sheet and power systems working.

--- module/config.js
+++ module/config.js
@@ -3,11 +3,11 @@ export const DIS = {};
 
 DIS.itemTypes = {
     armor: "armor",
-    cargo: "cargo",
     cosmicMutation: "cosmicMutation",
     equipment: "equipment",
     hubModule: "hubModule",
     origin: "origin",
     originBenefit: "originBenefit",
+    powerSystem: "powerSystem",
     weapon: "weapon"
 };
